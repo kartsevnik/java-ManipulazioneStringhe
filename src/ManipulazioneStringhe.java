34fca17b1c4c3e01e383e7c9cb7531dfc073e692
kartsevnik <kartsevnik@gmail.com>
2023-12-19 10:02:07 +0100

    add resolved method lowercase

--- src/ManipulazioneStringhe.java
+++ src/ManipulazioneStringhe.java
@@ -5,12 +5,24 @@ public class ManipulazioneStringhe {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Inserisci una stringa: ");
-        int stringa = scanner.nextInt();
+        String stringa = scanner.next();
 
-        String lunghezza = lunghezza(stringa);
+        int lunghezza = lunghezza(stringa);
         System.out.println("Lunghezza di stringa e: " + lunghezza);
 
+        String toLowercaseString = toLowercaseString(stringa);
+        System.out.println("String in lowercase: " + toLowercaseString);
+
         scanner.close();
+    }
+
+    public static int lunghezza(String stringa) {
+        int result = stringa.length();
+        return result;
+    }
+
+    public static String toLowercaseString(String stringa) {
+        return stringa.toLowerCase();
     }
 
 }
